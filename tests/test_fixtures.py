4e5b40d7480f8c07c9b84a030b8a86504ef270ca
SavinaDV <gvozdareva.darya@mail.ru>
2023-06-08 23:31:03 +0300

    Добавили файл с зависимостями.

---
+++ tests/test_fixtures.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture
+def browser():
+    print("Браузер стартует!")
+
+    yield
+
+    print("Браузер закрывается!")
+
+@pytest.fixture
+def chrome(browser):
+    pass
+
+
+@pytest.fixture
+def user_id():
+    return 123
+def test_auth(user_id, chrome):
+    assert user_id == 123
---
+++ tests/test_simple.py
@@ -0,0 +1,5 @@
+def test_login_page():
+    assert 1 == 2, "Неверный статус пользователя"
+
+def test_login_page():
+    assert 1 == 1, "верный статус пользователя"
